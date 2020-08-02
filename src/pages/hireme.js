1aed8d495fbcace9d388a588b2f04d6f214616e6
corbob <corbob@knoxy.ca>
2020-08-02 15:56:24 -0700

    .

---
+++ src/pages/hireme.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+import ExtLink from "../components/extLink"
+
+export default function Hireme() {
+    return (
+        <div style={{'text-align': `center`}}>
+        <h1> Cory Knox - Resume</h1>
+        <p>This is where a resume will be once I finish it. For now you can find it here: <ExtLink href="https://registry.jsonresume.com/corbob" text="json resume" />.</p>
+        <Link to="/">Home</Link>
+        </div>
+    )
+}
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,32 @@
 import React from "react"
+import ExtLink from "../components/extLink"
+import { Link } from "gatsby"
 
 export default function Home() {
     return (
-        <div>
-            <h1>Welcome</h1>
-            <p><img src="Cory.png" style={{width: '25%'}} /></p>
+        <div style={{'text-align': `center`}}>
+            <h1>Cory Knox</h1>
+            <p>
+                <img
+                    src="Cory.png"
+                    style={{ width: "30%" }}
+                    alt="ReverentGeek Style Avatar of Cory"
+                />
+            </p>
+            <ul>
+                <li>
+                    <ExtLink href="https://twitter.com/coryknox" text="Twitter" />
+                </li>
+                <li>
+                    <ExtLink href="https://github.com/corbob" text="GitHub" />
+                </li>
+                <li>
+                    <ExtLink href="https://linkedin.com/in/knoxcory" text="LinkedIn" />
+                </li>
+        <li>
+        <Link to="/hireme">Resume</Link>
+        </li>
+            </ul>
         </div>
     )
 }
