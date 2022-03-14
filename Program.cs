b20b68f1aa5fa0b29c6fbe7a261a5770ee13f0ad
Cory Knox <corbob@knoxy.ca>
2022-03-13 22:41:49 -0700

    Add some debugging.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
-﻿using Statiq.App;
+﻿using System.Diagnostics;
+using Statiq.App;
 using Statiq.Web;
+
+if (args.Where(val => val.Equals("--attach")).ToArray().Length != 0 && !Debugger.IsAttached)
+{
+    Console.WriteLine(string.Format("Waiting for a debugger for PID {0}", Process.GetCurrentProcess().Id));
+    while (!Debugger.IsAttached)
+    {
+        Thread.Sleep(250);
+    }
+}
 
 await Bootstrapper
 	.Factory
